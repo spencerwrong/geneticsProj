a41acdcec5f297a238a9e69ac5b468a77abe5cc2
wyu19 <wyu711@gmail.com>
2017-01-08 22:21:37 -0800

    need to finish testing the tree

--- Tester.java
+++ Tester.java
@@ -9,10 +9,34 @@ public class Tester
 	{
 		Scanner scan = new Scanner(System.in);
 		System.out.println("What color is your eye?");
+		
 		Tree user = new Tree(scan.next());
-		System.out.println("tree is created");
-		System.out.println("Your eye color is:"+user.getRoot().yourEyeColor);
+        
+
+		System.out.println("tree is created");                                               
+		System.out.println("Your eye color (root) is:"+user.getColor());
+	    
+	    //need to make a tracer that checks it but for somereason this is incompatible
+        user.getRoot()=user.getRoot().left;
+
+		
+
+        System.out.println("Your eye color (root) is:"+user.getColor());
 		scan.close();
 
 	}
 }
+
+                               
+
+//                           test this tree  make a node that traces around to follow and print each part.
+/*
+                                                 (n/a) (n/a)  
+                                                    \  /
+                                    (n/a)    (n/a) (blue)
+                                        \       \ /
+                         (n/a)   (n/a) (blue) (brown)
+                               \  /        \  /
+                             ( brown)   (  blue)
+                                  \      / 
+                                  ( blue )*/
--- Tree.java
+++ Tree.java
@@ -4,9 +4,9 @@ import java.util.Scanner;
 //put level into node so it can print which level you are in and what side;
 public class Tree 
 {
-	private static int lvlL =-1;
-    private static int lvlR =-1;
-	public class Node
+	public int leftLevel = 0;
+	public int rightLevel = 0;
+	public class Node      //remember to make private after testing
 	{
 		String yourEyeColor;
 		Node left;
@@ -16,24 +16,29 @@ public class Tree
 	  public Node(String EyeColor)
 	  {
 	  	System.out.println("inside Node construtor");
-		this.yourEyeColor = EyeColor;
+		this.yourEyeColor = EyeColor;                        //works as planned
 		System.out.println("eyeColor:"+this.yourEyeColor);
 
         if(EyeColor.compareTo("n/a")==0)
         {
 
-        	return ;
+        	System.out.println("you have entered n/a (we are in the Node constructor)");
+        	System.out.println(" ");
+        	return;
         }
-
-        lvlL++;
+       // System.out.println("About to begin askParents for left");
+       // leftLevel++;
+       // System.out.println("We are on left level:"+leftLevel);
 		left = askParents();
-		System.out.println("level for Left:"+lvlL);
+		
 		
 
-        lvlR++;
+      //  System.out.println("About to begin askParents right");
+      //  rightLevel++;
+      //System.out.println("We are on right level:"+rightLevel);
 		right = askParents();
 		
-		System.out.println("level for Right:"+lvlR);
+		
 	  }
     }
 
@@ -57,12 +62,22 @@ public class Tree
         return root;
 
      }
+
+     public String getColor()
+     {
+
+     	return root.yourEyeColor;
+     }
+     //Ask the user to 
+     //recursively keep making nodes until the user inputs "n/a"
+     //then go to the right side of the tree and make nodes and go back down  after the right side.
+
 	public Node askParents()
 	{
 		
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter the eye color for this slot if you do not know enter n/a");
-        
+        System.out.println("                 ");
       /*  Node x = new Node(color);
 
         String color = sc.next();
@@ -75,23 +90,48 @@ public class Tree
 		// create the parent node of the layer below you
 		// set nodes of parents
 		// create a node containing traits of parents
-        
+        String color = sc.next();
 
-		Node x = new Node(sc.next());
+       /* if(color.equals("n/a"))
+        {
+           System.out.println("colors is equal to n/a");
+           Node x = new Node(color);
+        }*/
+
+		Node x = new Node(color);
         System.out.println("Node for this slot has been created");
+        System.out.println("                     ");
+
 		
-	   if(x.yourEyeColor.compareTo("n/a")==0)           //base case is that the user doesnt know the eye color for that slot
+	  /* if(color.compareTo("n/a")==0)           //base case is that the user doesnt know the eye color for that slot
 		{
+			System.out.println("checked and found eyeColor == n/a");
 			return x;                //if the user cannot give you an answer to this slot then end the recursion and go to another slot
 		}
-
+        else
+        {
+        System.out.println("About to begin askParents for left");
+        leftLevel++;
+        System.out.println("We are on left level:"+leftLevel);
 		x.left = askParents();
+        leftLevel=0;
 		System.out.println("done with left side");
+		
+		System.out.println("                        ");
+		
+
+		System.out.println("About to begin askParents right");
+        rightLevel++;
+        System.out.println("We are on right level:"+rightLevel);
 		x.right = askParents();
+		rightLevel=0;
 		System.out.println("done with right side");
+
+		System.out.println("                        ");
 		sc.close();
 		return x;
-        
+        }*/
+        return x;
 	}
 }
 //ln -s /Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl
